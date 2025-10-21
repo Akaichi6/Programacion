928b1599ffc6bce83ba43b99147eaa5ece670ad0
ej clase <sanpalnav@alu.edu.gva.es>
2025-10-21 12:58:40 +0200

    ejercicios clase

---
+++ UD4/src/main/java/UD4/EJ1.java
@@ -0,0 +1,23 @@
+package UD4;
+
+public class EJ1 {
+    public static void main(String[] args) {
+        int dias = 365;
+        int horas = 24;
+        int minutos = 60;
+        int segundos = 60;
+
+        int horasanu = horas * dias;
+        int minutosanu = minutos * horasanu;
+        int segundosanu = segundos * minutosanu;
+        System.out.println(" Usando Println");
+        System.out.println("EL año tiene: " + horasanu + " horas");
+        System.out.println("El año tiene: " + minutosanu + " minutos");
+        System.out.println("EL año tiene: " + segundosanu + " segundos");
+
+        System.out.printf("Usando Printf\n");
+        System.out.printf("El año tiene: %1$,d horas \nEl año tiene: %2$,d minutos\nEl año tiene: %3$,d segundos ", horasanu, minutosanu, segundosanu);
+
+
+    }
+}
---
+++ UD4/src/main/java/UD4/EJ3.java
@@ -0,0 +1,7 @@
+package UD4;
+
+public class EJ3 {
+    public static void main(String[] args) {
+
+    }
+}

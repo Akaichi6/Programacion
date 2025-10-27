91f1e2f18db61c58a5f7897ad7c194fb978ab0bc
ej clase <sanpalnav@alu.edu.gva.es>
2025-10-27 13:49:03 +0100

    ejercicios clase

---
+++ UD4/src/main/java/UD4/EXTRA/EJ3.java
@@ -0,0 +1,31 @@
+package UD4.EXTRA;
+
+import java.util.Scanner;
+
+public class EJ3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del producto: ");
+        String nombre1 = sc.nextLine();
+        System.out.println("Ingrese la cantidad del producto: ");
+        String cantidad1 = sc.nextLine();
+        System.out.println("Ingrese el precio unitario del producto: ");
+        String precio1 = sc.nextLine();
+        System.out.println("Ingrese el nombre del producto: ");
+        String nombre2 = sc.nextLine();
+        System.out.println("Ingrese la cantidad del producto: ");
+        String cantidad2 = sc.nextLine();
+        System.out.println("Ingrese el precio unitario del producto: ");
+        String precio2 = sc.nextLine();
+        System.out.println("Ingrese el nombre del producto: ");
+        String nombre3 = sc.nextLine();
+        System.out.println("Ingrese la cantidad del producto: ");
+        String cantidad3 = sc.nextLine();
+        System.out.println("Ingrese el precio unitario del producto: ");
+        String precio3 = sc.nextLine();
+
+        System.out.printf("");
+
+
+    }
+}

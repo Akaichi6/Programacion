ea5c04e4ecc825b0941392752e6873f3c0c2ecfc
ej clase <sanpalnav@alu.edu.gva.es>
2025-12-01 10:16:51 +0100

    ej clase

---
+++ UD6/src/main/java/EJERCICIOS/EJ14.java
@@ -0,0 +1,32 @@
+package EJERCICIOS;
+
+import java.util.Arrays;
+
+public class EJ14 {
+    public static void main(String[] args) {
+
+
+        int tamanyo = 0;
+        int cantidad = 10;
+        for (int i = 1; i <= cantidad; i++) {
+            tamanyo += i;
+
+        }
+        int[] dimension = new int[tamanyo];
+        int k = 0;
+
+        for (int numero = 1; numero <= cantidad; numero++) {
+
+            for (int veces = 1; veces <= numero; veces++) {
+                dimension[k] = numero;
+                k++;
+            }
+        }
+
+        System.out.println(Arrays.toString(dimension));
+    }
+}
+
+
+
+

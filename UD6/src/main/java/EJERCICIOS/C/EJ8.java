ca050e3e5b7eb2815d50080913621b9702b22fe7
ej clase <sanpalnav@alu.edu.gva.es>
2025-12-02 13:49:31 +0100

    ej clase

---
+++ UD6/src/main/java/EJERCICIOS/C/EJ8.java
@@ -0,0 +1,24 @@
+package EJERCICIOS.C;
+
+import java.util.Arrays;
+
+public class EJ8 {
+    public static void main(String[] args) {
+
+        String paises [] = {"España, Alemania, Japon, Rusia"};
+        final int ALTURAMAX = 140;
+        final int ALTURAMIN = 210;
+
+        double[][] estaturas = new double[4][10];
+
+        for (int i = 0; i < paises.length; i++) {
+            System.out.println( paises[i] );
+            for (int j = 0; j < estaturas[i].length; j++) {
+                estaturas[i][j] = (int) (Math.random()  * (ALTURAMAX - ALTURAMIN )) + ALTURAMIN;
+                System.out.print(estaturas[i][j]);
+            }
+        }
+
+
+    }
+}

b4e34b52fe3c711aed2ebae9ce0c7b1a90d6b5e3
ej clase <sanpalnav@alu.edu.gva.es>
2025-12-02 12:38:33 +0100

    ej clase

--- UD6/src/main/java/EJERCICIOS/B/EJ9.java
+++ UD6/src/main/java/EJERCICIOS/B/EJ9.java
@@ -1,4 +1,18 @@
 package EJERCICIOS.B;
 
+import java.util.Scanner;
+
 public class EJ9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce una frase: ");
+        String frase = sc.nextLine();
+
+        System.out.println("introduce una vocal: ");
+        String vocal = sc.nextLine();
+
+        String cambio = frase.replace(vocal, vocal.toUpperCase());
+        System.out.println(cambio);
+    }
 }

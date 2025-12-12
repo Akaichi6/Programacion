9dc42681e390cce1a1ec6577dd7e14b54987814e
ej clase <sanpalnav@alu.edu.gva.es>
2025-12-12 10:31:18 +0100

    ej clase

--- UD6/src/main/java/EJERCICIOS/DINAMICAS/EJ3.java
+++ UD6/src/main/java/EJERCICIOS/DINAMICAS/EJ3.java
@@ -1,7 +1,21 @@
 package EJERCICIOS.DINAMICAS;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class EJ3 {
     public static void main(String[] args) {
-        
+        ArrayList<Integer> dimension1 = new ArrayList <>(List.of(15,10,25,33,10,15,18,19,14,17));
+        ArrayList<Integer> dimension2 = new ArrayList <>(dimension1);
+        ArrayList<Integer> dimension3 = new ArrayList <>(dimension1);
+        Collections.sort(dimension2);
+        Collections.sort(dimension3, Collections.reverseOrder());
+
+        System.out.println("Lista original: " + dimension1);
+        System.out.println("Lista ordenada de menor a mayor: " + dimension2);
+        System.out.println("Lista ordenada de mayor a menor:" + dimension3);
+
+
     }
 }

6cd56eccbe03bdbce82da15ffc54c5c6a2653617
ej clase <sanpalnav@alu.edu.gva.es>
2025-12-16 13:51:32 +0100

    ej clase

--- UD6/src/main/java/EJERCICIOS/DINAMICAS/CONJUNTOS/EJ5.java
+++ UD6/src/main/java/EJERCICIOS/DINAMICAS/CONJUNTOS/EJ5.java
@@ -1,16 +1,37 @@
 package EJERCICIOS.DINAMICAS.CONJUNTOS;
 
+import java.util.HashSet;
 import java.util.Scanner;
 import java.util.TreeSet;
 
 public class EJ5 {
     public static void main(String[] args) {
-        TreeSet<String> t1 = new TreeSet<>();
-
+        Scanner st = new Scanner(System.in);
+        HashSet<String> t1 = new HashSet<>();
+        
+        final String clave  = "fin";
         System.out.println("Introduce nombres (escribe 'fin' para terminar): ");
-
-        //while ()
-        Scanner sc = new Scanner(System.in);
-
+        String nombre =  st.nextLine();
+        
+        
+        
+        int intentos = 0;
+        while ( intentos == 0)
+        {
+            if (nombre.equals("fin"))
+            {
+                System.out.println("El programa acaba");
+                intentos++;
+            } else if (!nombre.equals("fin"))
+            {
+                t1.add(nombre);
+                nombre = st.nextLine();
+            }
+                
+        }
+        System.out.println("El conjunto de nombres no ordenados: " + t1);
     }
+
+
 }
+

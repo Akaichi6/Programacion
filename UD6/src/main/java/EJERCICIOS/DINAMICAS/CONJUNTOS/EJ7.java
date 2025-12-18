d273bab7304f3ca68a8f373217605aaa322d426f
ej clase <sanpalnav@alu.edu.gva.es>
2025-12-18 12:53:48 +0100

    ej clase

--- UD6/src/main/java/EJERCICIOS/DINAMICAS/CONJUNTOS/EJ7.java
+++ UD6/src/main/java/EJERCICIOS/DINAMICAS/CONJUNTOS/EJ7.java
@@ -1,20 +1,34 @@
 package EJERCICIOS.DINAMICAS.CONJUNTOS;
 
-import java.util.LinkedHashSet;
-import java.util.Scanner;
-import java.util.TreeSet;
+import java.util.*;
+
+import java.util.Collections;
+
 
 public class EJ7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         TreeSet<String> no_repetidos = new TreeSet<>();
         TreeSet<String> repetidos = new TreeSet<>();
-        TreeSet<String> vistos = new TreeSet<>();
+        Set<String> vistos = new TreeSet<>();
 
         System.out.println("Introduce una frase:");
         String frase = sc.nextLine().toLowerCase();
         String[] palabras = frase.split(" ");
+
+        List <String> miLista = new ArrayList<String>(vistos);
+
+        for (String palabra : palabras)
+        {
+
+        }
+
+        int repeticiones = Collections.frequency(miLista,palabras);
+
+
+    /*
         for (String palabra : palabras) {
+
             if (!vistos.add(palabra)) {
                 repetidos.add(palabra);
                 no_repetidos.remove(palabra);
@@ -26,6 +40,7 @@ public class EJ7 {
 
         System.out.println("Palabras repetidas: " + repetidos);
         System.out.println("Palabras no repetidas: " + no_repetidos);
+    */
 
     }
 

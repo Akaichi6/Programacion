cbba9fc436d19e46f7617efe601c3c7e4db9796a
ej clase <sanpalnav@alu.edu.gva.es>
2026-01-21 10:21:04 +0100

    ej clase

--- UD7/src/main/java/PRACTICA/NIVEL2_MEDIO/EJ15.java
+++ UD7/src/main/java/PRACTICA/NIVEL2_MEDIO/EJ15.java
@@ -1,12 +1,35 @@
 package PRACTICA.NIVEL2_MEDIO;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class EJ15 {
     public static void main(String[] args) {
 
         int [] numeros_reales = new int [100];
+        for (int i = 0; i < numeros_reales.length; i++) {
+            numeros_reales[i] = i+1;
+        }
+        System.out.println("La suma de los numeros reales es: " + suma(numeros_reales));
+        System.out.println("La media de los numeros reaes es: " + median(numeros_reales));
 
 
+    }
+    public static int suma (int [] numeros_reales) {
+        int suma = 0;
+        for (int i = 0; i < numeros_reales.length; i++) {
+            suma += numeros_reales[i];
+        }
+        return suma;
+    }
+    public static double median (int [] numeros_reales) {
+        double media = 0;
+        int suma = 0;
+        for (int i = 0; i < numeros_reales.length; i++) {
+            suma += numeros_reales[i];
+             media = (double) suma / numeros_reales.length;
+
+        }
+        return media;
     }
 }
---
+++ UD7/src/main/java/PRACTICA/NIVEL2_MEDIO/EJ16.java
@@ -0,0 +1,19 @@
+package PRACTICA.NIVEL2_MEDIO;
+
+import java.util.Scanner;
+
+public class EJ16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce lo que quieres que mida el array: ");
+        int num = sc.nextInt();
+        System.out.println("introduce el numero del que em peiecen los numeros aleatorios: ");
+        int min_aleatorio = sc.nextInt();
+
+        int [][] array = new int[num][2];
+
+
+    }
+
+}

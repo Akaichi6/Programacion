5431241d215b67f50dec376632118adee095e1f7
santi <sanpalnav@alu.edu.gva.es>
2026-01-22 19:57:50 +0100

    casa

---
+++ UD6/src/main/java/EXAMEN_DE_PRUEBA/EJ2.java
@@ -0,0 +1,47 @@
+package EXAMEN_DE_PRUEBA;
+
+import java.util.Scanner;
+
+public class EJ2 {
+    public static void main(String[] args) {
+        Scanner leer=new Scanner(System.in);
+
+            int aleatorio;
+            int filas;
+            int columnas;
+            int posicion_i=-1;
+            int posicion_j=-1;
+            int numero_menor=0;
+
+            System.out.print("Introduce el numero de filas: ");
+            filas=leer.nextInt();
+
+            System.out.print("Ingresa el numero de columnas: ");
+            columnas=leer.nextInt();
+
+            int[][] matriz=new int[filas][columnas];
+
+            for (int i=0;i<matriz.length;i++){
+
+                for (int j=0;j<columnas;j++){
+                    aleatorio=(int) (-1 + Math.random() * -10);
+                    matriz[i][j]=aleatorio;
+                }
+            }
+            for (int i=0 ; i <matriz.length; i++ ){
+
+                for (int j=0;j<columnas;j++){
+                    System.out.print(matriz[i][j]+"\t");
+
+                    if (matriz[i][j]<numero_menor){
+                        numero_menor=matriz[i][j];
+                        posicion_j=j;
+                        posicion_i=i;
+                    }
+                }
+                System.out.println("");
+            }
+        System.out.println("El Numero mas pequeño es: "+ numero_menor +" en la posicion ("+posicion_i+","+posicion_j+")");
+
+    }
+}

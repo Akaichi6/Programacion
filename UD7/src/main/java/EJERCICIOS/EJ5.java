cac624a4c7fd89e3390bf30f4ec6f9d3054fd631
santi <sanpalnav@alu.edu.gva.es>
2026-01-28 21:07:26 +0100

    casa

--- UD7/src/main/java/EJERCICIOS/EJ5.java
+++ UD7/src/main/java/EJERCICIOS/EJ5.java
@@ -8,39 +8,39 @@ public class EJ5 {
 
          ArrayList<String> empresas = new ArrayList<>(Arrays.asList("Uber","Apple","Ikea", "Oracle", "Iberdrola", "Endesa",
                  "Amazon", "Adobe" ));
-        System.out.println(empresas);
 
-        System.out.println(filtrarPorVocal(empresas));
+        System.out.println("Empresas agrupadas por vocal inicial y ordenadas alfabticamente: " );
+        filtrarPorVocal(empresas);
+
     }
 
-    public static Map<Character,String> filtrarPorVocal (ArrayList<String> empresas){
+    public static Map<Character, ArrayList> filtrarPorVocal (ArrayList<String> empresas){
 
         Map<Character,ArrayList> map=new HashMap<>();
 
         char [] vocales = {'a','e','i','o','u'};
-
 
         for (char c : vocales) {
             map.put(c, new ArrayList<>());
 
         }
         for (String empresa : empresas) {
-            char inicial = empresa.charAt(0);
-            if(empresas.contains(inicial)){
+            char inicial = Character.toLowerCase(empresa.charAt(0));
+
+            if(map.containsKey(inicial)){
                 map.get(inicial).add(empresa);
             }else {
                 map.put(empresa.charAt(0), new ArrayList());
             }
         }
-        System.out.println(Arrays.toString(map.keySet().toArray()));
 
+        for (List<String> lista : map.values()) {
+            Collections.sort(lista);
+        }
 
-
-
-
-
-
-
-        return Map.of();
+        for (char v : new char[]{'a','e','i','o','u'}) {
+            System.out.println(v + ": " + map.get(v));
+        }
+        return map;
     }
 }

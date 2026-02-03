482cb6075ba42e12de273228f9ee5cc9f9d6b649
ej clase <sanpalnav@alu.edu.gva.es>
2026-02-03 13:51:18 +0100

    ej clase

--- UD8/src/main/java/EJ4.java
+++ UD8/src/main/java/EJ4.java
@@ -1,6 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class EJ4 {
     static void main() {
-        System.out.println("introduce un valor: ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("introduce la cantidad de numeros que quepan en la dimension: ");
+        int n = sc.nextInt();
+        rellenarvector(n);
 
+    }
+    static void rellenarvector (int n){
+       Scanner sc = new Scanner(System.in);
+        int [] vector = new int[n];
+
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print("introduce un valor: ");
+            vector[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(vector));
     }
 }

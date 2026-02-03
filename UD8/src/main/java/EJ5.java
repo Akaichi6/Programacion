c0a9e6ed251b8a64df469ce4afeec2773bbcea86
ej clase <sanpalnav@alu.edu.gva.es>
2026-02-03 13:10:12 +0100

    ej clase

--- UD8/src/main/java/EJ5.java
+++ UD8/src/main/java/EJ5.java
@@ -5,54 +5,54 @@ import java.util.Scanner;
 
 public class EJ5 {
     public static void main(String[] args) {
+
+
+        int numeros[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        division(numeros);
+
+    }
+
+    public static double division(int [] numeros){
+
         Scanner sc = new Scanner(System.in);
 
-
+        double division = 0 ;
 
         try{
-            int numeros[] = {4, 5, 6, 7, 8, 9, 10};
-
             System.out.println(Arrays.toString(numeros));
             System.out.println("Introduce la posición del número que quieres: ");
-            int posicion1 = sc.nextInt();
+            int dividendo = sc.nextInt();
 
             System.out.println("Introduce la posición del número que quieres: ");
-            int posicion2 = sc.nextInt();
+            int divisor = sc.nextInt();
 
-            posiciones(numeros, posicion1, posicion2);
+            dividendo = numeros[dividendo -1];
+            System.out.println("Dividendo: " + dividendo);
+
+
+            divisor = numeros[divisor -1];
+            System.out.println("Divisor: " + divisor);
+
+            if (divisor != 0){
+                division = (double) dividendo / divisor;
+
+            } else  {
+                throw new ArithmeticException("La division no puede realizarse entre 0");
+            }
+
         }
         catch (InputMismatchException e){
             System.out.println("Valor introducido debe ser numérico");
         }
         catch (ArithmeticException e){
-            System.out.println("La division no puede realizarse entre 0");
+            System.out.println(e.getMessage());
         }
         catch (ArrayIndexOutOfBoundsException e){
             System.out.println("Indice fuera de su límite.");
         }
 
-    }
+        return division;
 
-
-    public static void posiciones(int [] numeros, int posicion1, int posicion2){
-
-        int numero1 = 0;
-        int numero2 = 0;
-
-        for (int i = 0; i < numeros.length; i++) {
-            if (posicion1 == i) {
-                numero1 = numeros[i]-1;
-                System.out.println("Dividendo: " + numero1);
-            }
-            if (posicion2 == i) {
-                numero2 = numeros[i]-1;
-                System.out.println("Divisor: " + numero2);
-            }
-
-        }
-
-        double division = (double) numero1 / numero2;
-        System.out.println(division);
     }
 
 }

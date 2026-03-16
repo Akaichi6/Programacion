40696f2ed01b8b91c23cf6a3f96c3901ec579a2f
ej clase <sanpalnav@alu.edu.gva.es>
2026-03-16 11:25:32 +0100

    ej clase

--- UD10/src/main/java/EJERCICIO_1/Camion.java
+++ UD10/src/main/java/EJERCICIO_1/Camion.java
@@ -1,4 +1,47 @@
 package EJERCICIO_1;
 
-public class Camion {
+public class Camion extends Vehiculo {
+    private double cargaMaxima;
+    private double cargaActual;
+
+    public Camion(String marca, String modelo, double velocidadMaxima, double cargaMaxima, double cargaActual) {
+        super(marca, modelo, velocidadMaxima);
+        this.cargaMaxima = cargaMaxima;
+        this.cargaActual = cargaActual;
+    }
+
+    @Override
+    public void acelerar(double velocidad) {
+        double mitadCarga = cargaMaxima/2;
+        if (cargaActual > mitadCarga) {
+            super.acelerar(velocidad);
+        }else {
+            super.acelerar(velocidad + 5);
+        }
+    }
+    public boolean superarCarga(){
+        return cargaActual < cargaMaxima;
+    }
+
+    @Override
+    public String mostrarInfo() {
+        return super.mostrarInfo() +
+                "\ncargaMaxima: " + cargaMaxima
+                + "\ncargaActual: " + cargaActual;
+    }
+    public double getCargaMaxima() {
+        return cargaMaxima;
+    }
+
+    public void setCargaMaxima(double cargaMaxima) {
+        this.cargaMaxima = cargaMaxima;
+    }
+
+    public double getCargaActual() {
+        return cargaActual;
+    }
+
+    public void setCargaActual(double cargaActual) {
+        this.cargaActual = cargaActual;
+    }
 }
--- UD10/src/main/java/EJERCICIO_1/Coche.java
+++ UD10/src/main/java/EJERCICIO_1/Coche.java
@@ -7,7 +7,21 @@ public class Coche extends Vehiculo {
         super(marca, modelo, velocidadMaxima);
         this.num_puertas = num_puertas;
     }
+    @Override
     public void acelerar(double velocidad) {
         super.acelerar(velocidad + 15);
+    }
+    @Override
+    public String mostrarInfo(){
+        return super.mostrarInfo() +
+                "\n Numero de puertas: " + num_puertas;
+    }
+
+    public int getNum_puertas() {
+        return num_puertas;
+    }
+
+    public void setNum_puertas(int num_puertas) {
+        this.num_puertas = num_puertas;
     }
 }
